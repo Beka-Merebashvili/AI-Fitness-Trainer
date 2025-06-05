05971802820cb5e267daa79a7a7b02d7d38da84e
Beka-Merebashvili <beqamerebashvili26@gmail.com>
2025-06-05 14:43:43 +0400

    finish authentication

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, SignOutButton } from '@clerk/nextjs'
 import React from 'react'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <div>
+      Homepage
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+
+      <SignedIn>
+        <SignOutButton />
+      </SignedIn>
+    </div>
   )
 }
 
